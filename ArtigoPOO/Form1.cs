51f7fd2a5575d58a57009c04bdbe787e3b360cdb
Moises Alves <87319078+mooisesalves1@users.noreply.github.com>
2022-08-11 16:05:26 -0300

    Artigo sobre Orientação a Objetos

---
+++ ArtigoPOO/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArtigoPOO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAtribuirValores_Click(object sender, EventArgs e)
+        {
+            //Instanciando um objeto do tipo produto
+            Produto objetoProduto = new Produto();
+
+            //Atribuindo valores a suas propriedades
+            objetoProduto._codigo = 2055;
+            objetoProduto._nome = "Refrigerante Laranja Lata 350ml";
+            objetoProduto._preco_compra = 1.75m;
+            objetoProduto._preco_venda = 2.5m;
+            objetoProduto._quantidade_estoque = 50;
+            objetoProduto._ativo = true;
+            objetoProduto._data_cadastro = DateTime.Now;
+
+            //Mostrando informações do produto na tela
+            MessageBox.Show("Informações do produto:" + "\n" +
+                "Código: " + objetoProduto._codigo + "\n" +
+                "Nome: " + objetoProduto._nome + "\n" +
+                "Preço de compra : " + objetoProduto._preco_compra + "\n" +
+                "Preço de venda: " + objetoProduto._preco_venda + "\n" +
+                "Qtde estoque: " + objetoProduto._quantidade_estoque + "\n" +
+                "Status produto: " + (objetoProduto._ativo == true ? "Ativo" : "Inativo") + "\n" +
+                "Data cadastro: " + objetoProduto._data_cadastro);                
+        }
+    }
+}
---
+++ ArtigoPOO/Produto.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtigoPOO
+{
+    public class Produto
+    {
+        private int codigo;
+
+        public int _codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+        private string nome;
+
+        public string _nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        private decimal preco_compra;
+
+        public decimal _preco_compra
+        {
+            get { return preco_compra; }
+            set { preco_compra = value; }
+        }
+
+        private decimal preco_venda;
+
+        public decimal _preco_venda
+        {
+            get { return preco_venda; }
+            set { preco_venda = value; }
+        }
+
+        private int quantidade_estoque;
+
+        public int _quantidade_estoque
+        {
+            get { return quantidade_estoque; }
+            set { quantidade_estoque = value; }
+        }
+
+        private bool ativo;
+
+        public bool _ativo
+        {
+            get { return ativo; }
+            set { ativo = value; }
+        }
+
+        private DateTime data_cadastro;
+
+        public DateTime _data_cadastro
+        {
+            get { return data_cadastro; }
+            set { data_cadastro = value; }
+        }
+
+
+        public int inserir(Produto objNovoProduto)
+        {
+            //***Código para inserir um novo produto***
+
+            return 20;
+        }
+
+        public bool alterar(Produto objAlterarProduto)
+        {
+            //***Código para alterar um produto***
+
+            if (true)//Um teste verificando se o produto foi atualizado. 
+            {
+                //Retorna "true" se obtiver sucesso na alteração.
+                return true;
+            }
+            else
+            {
+                //Retorna "false" se não obtiver sucesso na alteração.
+                return false;
+            }
+        }
+
+        public void excluir(int codExcluirProduto)
+        { 
+            //***Realiza a exclusão do produto***
+        }
+
+        public List<Produto> pesquisar(int codProduto, string nomeProduto)
+        {
+            //Cria um um objeto do tipo lista de produtos
+            List<Produto> listaProdutoRertono = new List<Produto>();
+
+            if (codProduto > 0)
+            {
+                //***Pesquisa produto pelo código***
+
+                return listaProdutoRertono;
+            }
+            else
+            {
+                //***Pesquisa produto pelo nome***
+
+                return listaProdutoRertono;
+            }
+        }
+
+    }
+}
